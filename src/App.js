c7bfd7128cb7a4fbba925affed47e82e7a57bfdd
MyakaArchana <89454131+MyakaArchana@users.noreply.github.com>
2022-01-27 18:28:10 +0530

    achhuummmm

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+import * as mui from '@mui/material'
+import * as Icons from '@mui/icons-material'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <div className="price-table">
+          <div className="title">
+            <h2>Free</h2>
+            <b>$0/month</b>
+          </div>
+          <div className="content">
+            <div className="line"> <Icons.Done /><p>Single User</p></div>
+            <div className="line"> <Icons.Done /><p>5GB Storage</p></div>
+            <div className="line"> <Icons.Done /><p>Unlimited Public Projects</p></div>
+            <div className="line"> <Icons.Done /><p>Community Access</p></div>
+            <div className="muted-line"> <Icons.Close /><p>Unlimited Private Projects</p></div>
+            <div className="muted-line"> <Icons.Close /><p>Dedicated Phone Support</p></div>
+            <div className="muted-line"> <Icons.Close /><p>Free Subdomain</p></div>
+            <div className="muted-line"> <Icons.Close /><p>Monthly Status Reports</p></div>
+          </div>
+          <mui.Button color="secondary" href="#" sx={{width:"100%", minHeight:"50px", borderRadius:"0px"}}  variant="contained">Buy</mui.Button>  
+        </div>
+        <div className="price-table">
+        <div className="title" style={{backgroundColor:'#fe2100'}}>
+            <h2>Plus</h2>
+            <b>$9/month</b>
+          </div>
+          <div className="content">
+            <div className="line"> <Icons.Done /><p>5 User</p></div>
+            <div className="line"> <Icons.Done /><p>50GB Storage</p></div>
+            <div className="line"> <Icons.Done /><p>Unlimited Public Projects</p></div>
+            <div className="line"> <Icons.Done /><p>Community Access</p></div>
+            <div className="line"> <Icons.Done /><p>Unlimited Private Projects</p></div>
+            <div className="line"> <Icons.Done /><p>Dedicated Phone Support</p></div>
+            <div className="line"> <Icons.Done /><p>Free Subdomain</p></div>
+            <div className="muted-line"> <Icons.Close /><p>Monthly Status Reports</p></div>
+          </div>
+          <mui.Button color="secondary"  href="#" sx={{width:"100%", minHeight:"50px", borderRadius:"0px"}}  variant="contained">Buy</mui.Button>
+        </div>
+        <div className="price-table">
+        <div className="title" style={{backgroundColor:'green'}}>
+            <h2>Pro</h2>
+            <b>$49/month</b>
+          </div>
+          <div className="content">
+            <div className="line"> <Icons.Done /><p><b>Unlimited User</b></p></div>
+            <div className="line"> <Icons.Done /><p>150GB Storage</p></div>
+            <div className="line"> <Icons.Done /><p>Unlimited Public Projects</p></div>
+            <div className="line"> <Icons.Done /><p>Community Access</p></div>
+            <div className="line"> <Icons.Done /><p>Unlimited Private Projects</p></div>
+            <div className="line"> <Icons.Done /><p>Dedicated Phone Support</p></div>
+            <div className="line"> <Icons.Done /><p><b>Unlimited</b> Free Subdomain</p></div>
+            <div className="line"> <Icons.Done /><p>Monthly Status Reports</p></div>
+          </div>
+          <mui.Button color="secondary"  href="#" sx={{width:"100%", minHeight:"50px", borderRadius:"0px"}}  variant="contained">Buy</mui.Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
